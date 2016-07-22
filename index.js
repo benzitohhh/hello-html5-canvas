4abdef2a964658d318412ebabcbfc3fdec1da6d1
Ben Immanuel <ben.immanuel@aistemos.com>
2016-07-22 15:37:45 +0100

    Revert "and yet more changes"
    This reverts commit 42028998b4006a3fe1b2810842392e8bce163ecd.

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-var MAX_SQUARES = 200;
-
-var MAX_WIDTH = 1000;
-var MIN_WIDTH = 30;
-
-var MAX_HEIGHT = 750;
-var MIN_HEIGHT = 30;
-
-var MIN_INDENT = 3;
-var MAX_INDENT = 60;
-
 function as_degree(rad) {
   return 360 / (2 * Math.PI) * rad;
 }
@@ -41,55 +30,45 @@ function get_random_int(min, max) {
   return min + Math.floor(Math.random() * (max - min));
 }
 
-function _draw(i, ctx, indent, num_squares, width, color_scale) {
-  if (i >= num_squares) {
-    return;
-  }
+var MAX_SQUARES = 200;
 
-  // fill/stroke
-  ctx.strokeRect(0, 0, width, width);
-  ctx.fillStyle = color_scale(i);
-  ctx.fillRect(0, 0, width, width);
+var MAX_WIDTH = 1000;
+var MIN_WIDTH = 30;
 
-  // Get next values
-  var next_angle = get_next_angle(width, indent);
-  var next_width = get_next_width(width, indent);
+var MIN_INDENT = 3;
+var MAX_INDENT = 60;
 
-  // translate to rotation centre and stroke
-  ctx.translate(indent, 0);
-  ctx.rotate(next_angle);
+var INDENT = get_random_int(MIN_INDENT, MAX_INDENT);
+var INITIAL_WIDTH =  get_random_int(MIN_WIDTH, MAX_WIDTH); // MAX_WIDTH;
+var END_WIDTH = get_random_int(MIN_WIDTH, INITIAL_WIDTH);
 
-  _draw(i+1, ctx, indent, num_squares, next_width, color_scale);
-}
-
-function draw(ctx, start_x, start_y, indent, initial_width, start_angle, num_squares, color_scale) {
-  ctx.save();
-  ctx.translate(start_x, start_y);
-  ctx.rotate(start_angle);
-  _draw(0, ctx, indent, num_squares, initial_width, color_scale);
-  ctx.restore();
-}
-
-var indent = get_random_int(MIN_INDENT, MAX_INDENT);
-var initial_width = get_random_int(MIN_WIDTH, MAX_WIDTH);
-// var initial_width = MAX_WIDTH;
-var end_width = get_random_int(MIN_WIDTH, initial_width);
-//var initial_angle = Math.random() * Math.PI / 2;
-var initial_angle = Math.PI / 4 - Math.random() * Math.PI / 2;
-
-// var start_x = get_random_int(0, MAX_WIDTH);
-// var start_y = get_random_int(0, MAX_HEIGHT / 2);
-
-var start_x = 0;
-var start_y = 0;
-
-var num_squares = get_num_squares(initial_width, end_width, indent);
+var num_squares = get_num_squares(INITIAL_WIDTH, END_WIDTH, INDENT);
 
 // colour gradient
 var color_scale = d3.scale.linear()
       .domain([0, num_squares - 1])
       .range(["red", "yellow"]);
 
-var ctx = document.getElementById('canvas').getContext('2d');
+function draw() {
+  var ctx = document.getElementById('canvas').getContext('2d');
 
-draw(ctx, start_x, start_y, indent, initial_width, initial_angle, num_squares, color_scale);
+  var width = INITIAL_WIDTH;
+  var angle = 0;
+
+  for(var i = 0; i < num_squares; i++) {
+    // fill/stroke
+    ctx.strokeRect(0, 0, width, width);
+    ctx.fillStyle = color_scale(i);
+    ctx.fillRect(0, 0, width, width);
+
+    // Get next values
+    angle = get_next_angle(width, INDENT);
+    width = get_next_width(width, INDENT);
+
+    // translate to rotation centre and stroke
+    ctx.translate(INDENT, 0);
+    ctx.rotate(angle);
+  }
+
+
+}
